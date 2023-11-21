a8b5b9d41bd2780cff49fd53a2093e70210417b5
Sagor002 <136358823+Sagor002@users.noreply.github.com>
2023-11-21 21:47:32 +0600

    Add files via upload
    all done

---
+++ app.js
@@ -0,0 +1,23 @@
+// first Question Answer
+let feedTime = "morning";
+    feedTime = "afternoon";
+
+console.log(feedTime)
+
+
+// second Questions Answer
+const maximumWeight = "500KG";
+const boatWeight = "150KG"
+const passengersWeight = "340KG";
+if (passengersWeight <= maximumWeight) {
+    console.log("Passengers and boat can cross the river safely.");
+} else {
+    console.log("Passengers and boat can't cross the river.");
+}
+
+
+// third Questions Answer
+const myName = "Abdur Nur";
+const Hobby = "the Football playing";
+const sentence = `Hi, my name is ${myName}. My favorite hobby is ${Hobby}.`;
+console.log(sentence);
